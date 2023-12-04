3cdf9c136ce80135f50f454f692861eae88f65ec
OlvDavid <146491742+OlvDavid@users.noreply.github.com>
2023-12-04 14:42:03 -0300

    Atualização do Codigo
    Nesse commit adicionei o função do Menu Principal, onde o usuário ira escolher entre entrar no programa e fazer o pedido ou sair

--- Quiosque-Autoatendimento.cpp
+++ Quiosque-Autoatendimento.cpp
@@ -1,6 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+struct Cliente{
+	char name[25];	
+
+	};
+
+int menuPrincipal(){
+
+	struct Cliente cliente;
+	
+	int options;
+	
+	printf("|====================|====================|");
+  	printf("\n|                    |                    |");
+  	printf("\n|  1- FAZER PEDIDO   |     2- SAIR        |");
+  	printf("\n|                    |                    |");
+  	printf("\n|====================|====================|");
+  	printf("\nESCOLHA UMA OPÇÃO: ");
+  	scanf("%d", &options);
+	
+	switch(options){
+		case 1:
+			system("cls");
+			
+			printf("=============CADASTRO DE CLIENTE==============");
+			printf("\nInforme seu nome para seguirmos com o pedido\n\t\t");
+			scanf("%s", &cliente.name);
+			break;
+			
+		case 2:
+			system("cls");
+			
+			printf("Ate a proxima!!!\n");
+			
+			exit(0);
+			break;
+		
+		default:
+			system("cls");
+			
+			printf("Opção invalida. Tente Novamente.\n");
+			
+			system("pause");
+			return menuPrincipal();
+			break;
+	}
+
+	}
 
 int main() {
+
+	setlocale(LC_ALL, "portuguese");
 	
+	menuPrincipal();	
 	
 }
